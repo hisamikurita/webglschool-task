483a6aa191c54f0115354d515f9df14c5d95bc7e
Hisami Kurita <47776346+hisamikurita@users.noreply.github.com>
2020-05-05 14:58:17 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -20,6 +20,13 @@
         }, false);
         run = true;
 
+        // リサイズイベントの定義
+        window.addEventListener('resize', () => {
+            renderer.setSize(window.innerWidth, window.innerHeight);
+            camera.aspect = window.innerWidth / window.innerHeight;
+            camera.updateProjectionMatrix();
+        }, false);
+
         render();
     });
 
